11aaf11e4accf8d72a76c8336ccbeb5b39ace249
Adam Linkhart <alinkhart@pureromance.com>
2018-05-09 16:11:22 -0400

    White space change

--- StrawMan/App.cs
+++ StrawMan/App.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿dusing System;
 
 namespace StrawMan {
     public class App {
         public static bool UseMockDataStore = true;
         public static string BackendUrl = "http://localhost:5000";
 
-        public static void Initialize() {
+        public static void Initialize() { 
             if (UseMockDataStore)
                 ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
             else
